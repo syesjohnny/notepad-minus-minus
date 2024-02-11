7de7edaed27e1053adf1bb2626ed49c761c5e386
johnny263 <63039021+syesjohnny@users.noreply.github.com>
2024-02-11 21:59:57 +0800

    create project

---
+++ notepad-minus-minus/notepad-minus-minus/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace notepad_minus_minus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
